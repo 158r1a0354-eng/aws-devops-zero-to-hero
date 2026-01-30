15e5359eff7dfb5126f1b281a95d05140deb5672
158r1a0354-eng <158r1a0354@gmail.com>
2026-01-30 14:16:40 +0530

    Simplify container stop and remove commands

--- stop_container.sh
+++ stop_container.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e
 
-CONTAINER_NAME=flask-app
-
-echo "Stopping container..."
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
-
-echo "Container stopped"
+docker stop flask-app || true
+docker rm flask-app || true
